bb157dfe57b9f83b64a7380c247a2342c55b9795
Shakira <287145569+lilywinnie@users.noreply.github.com>
2026-07-28 23:17:08 +0600

    Create ans4.cpp

---
+++ ans4.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int T;
+    cin>>T;
+    
+    while(T--){
+        
+        int N;
+        cin>>N;
+        
+        int L,H;
+        
+        L= (N/2)/2;
+        H=(N/2)-L;
+        
+        cout<<L*H<<"\n";
+     
+    }
+
+}
