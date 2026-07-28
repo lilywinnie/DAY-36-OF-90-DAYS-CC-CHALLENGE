84f02cec43c6fd8c0c616ab5bce6363b2f395607
Shakira <287145569+lilywinnie@users.noreply.github.com>
2026-07-28 20:45:27 +0600

    Update ans1.cpp

--- ans1.cpp
+++ ans1.cpp
@@ -6,7 +6,11 @@ int main() {
     int N1, N2;
     cin>>N1>>N2;
     
-    cout<<N1-N2<<"\n";
+    if(N1>N2)
+        cout<<N1-N2<<"\n";
+        
+    else
+        cout<<N1+N2<<"\n";
     
     return 0;
 }
