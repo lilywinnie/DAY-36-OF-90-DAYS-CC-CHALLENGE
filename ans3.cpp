f62258c736e5d7461591391311e5eb789b7ef44c
Shakira <287145569+lilywinnie@users.noreply.github.com>
2026-07-28 23:00:09 +0600

    Create ans3.cpp

---
+++ ans3.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int T;
+    cin>>T;
+    
+    while(T--){
+        
+        int A,B,C;
+        cin>>A>>B>>C;
+        
+        if(A+B+C==180)
+            cout<<"YES\n";
+            
+        else
+            cout<<"NO\n";
+     
+    }
+    
+    return 0;
+
+}
